8fcda59a13e967e638a9eb04e3caf101c9199d57
HarshikeshKumar <harshikeshkoili214@gmail.com>
2026-05-08 19:16:51 +0530

    This is my frist Intellij Idea Project

---
+++ My First Application/src/MyFirstApp.java
@@ -0,0 +1,6 @@
+public class MyFirstApp {
+    public static void main(String[] args) {
+        System.out.println("Hello, This is My First Java Code in Intellij Idea");
+        System.out.println("Hii, I'm Harshikesh kumar");
+    }
+}
